9e3392c3ef0ace122ca6a842e657e1c5985b1ba9
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-24 15:06:12 -0400

    Added Tab Descriptor router with dummy data

--- front-end/src/Root.tsx
+++ front-end/src/Root.tsx
@@ -11,16 +11,23 @@ const Root = () => {
     ];
     return (
         <>
-            <div className="flex sticky top-0 z-50 justify-between bg-neutral">
+            <div className="flex sticky top-0 z-50 justify-between bg-neutral p-2">
                 <div className="flex">
-                    <a className="text-xl p-2">Styling Basics</a>
+                    <a className="text-xl p-2 text-secondary-content">
+                        Styling Basics
+                    </a>
                 </div>
                 <div className="flex-none">
                     <ul className="flex">
                         {arr.map((x, id) => {
                             return (
                                 <li key={id} className="p-2">
-                                    <a href={x.path}>{x.name}</a>
+                                    <a
+                                        href={x.path}
+                                        className="text-secondary-content"
+                                    >
+                                        {x.name}
+                                    </a>
                                 </li>
                             );
                         })}
--- front-end/src/routes/RouteTabDescriptor.tsx
+++ front-end/src/routes/RouteTabDescriptor.tsx
@@ -1,7 +1,94 @@
-const RouteTabDescriptor = () => {
+import { useState } from "react";
+
+import Tab from "../components/Tab";
+import userImage0 from "../assets/user2.jpg";
+import userImage1 from "../assets/user1.jpg";
+import userImage2 from "../assets/profile.jpg";
+
+interface tab {
+    userName: string;
+    userId: number;
+    userImage: string;
+    userBio: string;
+}
+
+interface iTabSelector {
+    userId: number;
+    userName: string;
+    handleSetTab: (userId: number) => void;
+}
+
+const data = [
+    {
+        userName: "Nicki",
+        userId: 0,
+        userImage: userImage0,
+        userBio:
+            "A 28-year-old software engineer who loves AI, hiking, and sci-fi novels. Enjoys traveling and trying new recipes.",
+    },
+
+    {
+        userName: "Jake",
+        userId: 1,
+        userImage: userImage1,
+        userBio:
+            "A 35-year-old graphic designer who creates digital art, plays guitar, and practices yoga. Tech and gaming enthusiast.",
+    },
+
+    {
+        userName: "Daniel",
+        userId: 2,
+        userImage: userImage2,
+        userBio:
+            "A 29-year-old amateur chess player who enjoys gelato. Enjoys building and ruining friendships on the pickleball court.",
+    },
+];
+
+const TabSelector: React.FC<iTabSelector> = ({
+    userId,
+    userName,
+    handleSetTab,
+}) => {
     return (
-        <div id="RouteTabDescriptor">
-            <div className="bg-white">Tab Descriptor</div>
+        <div className="p-2 px-4 font-medium">
+            <button onClick={() => handleSetTab(userId)}>{userName}</button>
+        </div>
+    );
+};
+
+const RouteTabDescriptor = () => {
+    const [tab, setTab] = useState<tab>(data[0]);
+
+    const handleSetTab = (userId: number) => {
+        const newTab = data.filter((x) => x.userId === userId)[0];
+        setTab(newTab);
+    };
+
+    return (
+        <div
+            id="RouteTabDescriptor"
+            className="h-screen bg-base-200 flex justify-center"
+        >
+            <div className="basis-1/2 py-10">
+                <div className="flex justify-between p-2 bg-base-100 rounded-lg">
+                    {data.map((x, id) => {
+                        return (
+                            <TabSelector
+                                key={id}
+                                userId={x.userId}
+                                userName={x.userName}
+                                handleSetTab={handleSetTab}
+                            />
+                        );
+                    })}
+                </div>
+                <br />
+                <Tab
+                    userId={tab.userId}
+                    userImage={tab.userImage}
+                    userBio={tab.userBio}
+                />
+            </div>
         </div>
     );
 };
